0895f956a8848377deb9968d514978721152caae
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2023-12-08 17:00:49 +0000

    feat: NFT to fungible token manipulation

---
+++ solidity/nft_token.sol
@@ -0,0 +1,203 @@
+import "./library/spl_token.sol";
+import "./library/system_instruction.sol";
+
+struct Position {
+    address whirlpool;      // 32
+    address position_mint;  // 32
+    uint128 liquidity;      // 16
+    int32 tick_lower_index; // 4
+    int32 tick_upper_index; // 4
+}
+
+contract nft_token {
+    address public orca;
+    address public whirlpool;
+    address public pdaProgram;
+    address public mintErc20;
+    address public pdaERC20Account;
+    // TODO: Change for the latter one in production
+    uint64 public pdaHeader = 0x55bdfe33;//0xd0f7407ae48fbcaa;
+    // PDA seed
+    bytes public constant pdaProgramSeed = "pdaProgram";
+    //address constant tokenProgram = address"TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
+
+    @payer(payer)
+    constructor(address _orca, address _whirlpool, address _pdaProgram, address _mintErc20, address _pdaERC20Account) {
+        orca = _orca;
+        whirlpool = _whirlpool;
+        pdaProgram = _pdaProgram;
+        mintErc20 = _mintErc20;
+        pdaERC20Account = _pdaERC20Account;
+    }
+
+    function getPositionData(address positionDataAccount, address positionMint) public view returns (Position position) {
+        // Check for the
+        for (uint64 i = 0; i < tx.accounts.length; i++) {
+            AccountInfo ai = tx.accounts[i];
+            if (ai.key == positionDataAccount) {
+                // TODO Shift everything left by 8 bytes in production
+                position = Position({
+                    whirlpool: ai.data.readAddress(16),
+                    position_mint: ai.data.readAddress(48),
+                    liquidity: ai.data.readUint128LE(80),
+                    tick_lower_index: ai.data.readInt32LE(96),
+                    tick_upper_index: ai.data.readInt32LE(100)
+                });
+
+                // Check the whirlpool
+                if (position.whirlpool != whirlpool) {
+                    revert("Wrong whirlpool address");
+                }
+
+                // Check the NFT address
+                if (position.position_mint != positionMint) {
+                    revert("Wrong NFT address");
+                }
+
+                // Check the PDA ownership
+                // TODO: Uncomment following lines in production
+//                if (ai.owner != orca) {
+//                    revert("Wrong pda owner");
+//                }
+
+                // Check the PDA header data
+                uint64 header = ai.data.readUint64LE(0);
+                if (header != pdaHeader) {
+                    revert("Wrong pda header");
+                }
+
+                // Check the PDA address correctness
+                (address pdaPosition, ) = try_find_program_address(["position", position.position_mint], orca);
+                // TODO: Uncomment following lines in production
+//                if (pdaPosition != positionDataAccount) {
+//                    revert("Wrong position pdaPosition");
+//                }
+
+                return position;
+            }
+        }
+
+        revert("account missing");
+    }
+
+    @mutableAccount(fromTokenAccount)
+    @mutableAccount(pdaTokenAccount)
+    @mutableAccount(toErc20)
+    @mutableAccount(mintERC20)
+    @mutableAccount(pdaProgram)
+    @account(positionDataAccount)
+    @signer(fromWallet)
+    function deposit(address positionMint, bytes bump) external {
+        AccountInfo ai = tx.accounts.positionDataAccount;
+        // TODO Shift everything left by 8 bytes in production
+        Position position = Position({
+            whirlpool: ai.data.readAddress(16),
+            position_mint: ai.data.readAddress(48),
+            liquidity: ai.data.readUint128LE(80),
+            tick_lower_index: ai.data.readInt32LE(96),
+            tick_upper_index: ai.data.readInt32LE(100)
+        });
+
+        // Check the whirlpool
+        if (position.whirlpool != whirlpool) {
+            revert("Wrong whirlpool address");
+        }
+
+        // Check the NFT address
+        if (position.position_mint != positionMint) {
+            revert("Wrong NFT address");
+        }
+
+        // Check the PDA ownership
+        // TODO: Uncomment following lines in production
+//                if (ai.owner != orca) {
+//                    revert("Wrong pda owner");
+//                }
+
+        // Check the PDA header data
+        uint64 header = ai.data.readUint64LE(0);
+        if (header != pdaHeader) {
+            revert("Wrong pda header");
+        }
+
+        // Check the PDA address correctness
+        (address pdaPosition, ) = try_find_program_address(["position", position.position_mint], orca);
+        // TODO: Uncomment following lines in production
+//                if (pdaPosition != positionDataAccount) {
+//                    revert("Wrong position pda");
+//                }
+
+        // TODO: Do the liquidity check for max(uint64) value as it is provided as uint128 from the LP provider
+
+        // Transfer the NFT to the pdaTokenAccount address of this program
+        SplToken.transfer(
+            tx.accounts.fromTokenAccount.key,
+            tx.accounts.pdaTokenAccount.key,
+            tx.accounts.fromWallet.key,
+            1);
+
+        // Transfer ERC20 tokens to the user
+        SplToken.mint_to_pda(
+            mintErc20,
+            tx.accounts.toErc20.key,
+            pdaProgram,
+            uint64(position.liquidity),
+            pdaProgramSeed,
+            bump);
+    }
+
+    @mutableAccount(fromERC20Account)
+    @mutableAccount(pdaERC20Account)
+    @mutableAccount(fromWallet)
+    @mutableAccount(pdaTokenAccount)
+    @mutableAccount(fromTokenAccount)
+    @mutableAccount(pdaProgram)
+    @mutableAccount(mintERC20)
+    @signer(sig)
+    // Transfer with PDA
+    function withdraw(uint64 amount, bytes bump) external {
+        // Transfer ERC20 tokens to the pdaERC20Account address of this program
+        SplToken.transfer(
+            tx.accounts.fromERC20Account.key,
+            tx.accounts.pdaERC20Account.key,
+            tx.accounts.fromWallet.key,
+            amount);
+
+        // Transfer NFT to the user associated token account
+        SplToken.transfer_pda(
+            tx.accounts.pdaTokenAccount.key,
+            tx.accounts.fromTokenAccount.key,
+            pdaProgram,
+            1,
+            pdaProgramSeed,
+            bump);
+
+        // Burn acquired ERC20 tokens
+        SplToken.burn_pda(tx.accounts.pdaERC20Account.key, mintErc20, pdaProgram, amount, pdaProgramSeed, bump);
+    }
+
+//    @mutableAccount(toErc20)
+//    @mutableAccount(mintERC20)
+//    @mutableAccount(pdaProgram)
+//    @signer(owner)
+//    function mint(uint64 amount, bytes bump) external {
+//        // Transfer ERC20 tokens to the user
+//        SplToken.mint_to_pda(
+//            mintErc20,
+//            tx.accounts.toErc20.key,
+//            pdaProgram,
+//            amount,
+//            pdaProgramSeed,
+//            bump);
+//    }
+
+    @account(account)
+    function get_balance() external view returns (uint64) {
+        return SplToken.get_balance(tx.accounts.account);
+    }
+
+    @account(account)
+    function totalSupply() external view returns (uint64) {
+        return SplToken.total_supply(tx.accounts.account);
+    }
+}
